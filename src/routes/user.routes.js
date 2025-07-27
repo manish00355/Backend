f62751d612d5c410d2d1ee7a16424e55954f8562
manish00355 <manny00355@gmail.com>
2025-07-27 16:58:35 +0530

    add user profile and password management routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAcessToken } from "../controllers/user.controller";
+import { loginUser, logoutUser, registerUser,refreshAcessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, getUserChannelProfile } from "../controllers/user.controller";
 
 import { upload } from "../middlewares/multer.middleware";
 import { verifyJWT } from "../middlewares/auth.middleware";
@@ -29,8 +29,19 @@ router.route("/logout").post( verifyJWT ,logoutUser)
 
 router.route("/refresh-token").post(refreshAcessToken)
 
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 
+router.route("/current-user").get(verifyJWT,getCurrentUser)
 
+router.route("/update-profile").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateAccountDetails)
 
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserAvatar)
+
+// params 
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 export default router
